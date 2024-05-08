da184598f7020f6b1e9c64a709e4aa56df1377cf
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-05-08 18:27:22 +0200

    Testing card importing into postgres

---
+++ backend/cardParser.py
@@ -0,0 +1,154 @@
+import re
+import datetime as dt
+
+def matchTrait(trait: str, source: str):
+    return re.search(r'"'+ trait + r'":"(.*?)",', source)
+
+def getTrait(trait: str, source: str) -> str:
+    traitMatch = matchTrait(trait, source)
+    if traitMatch != None:
+        return traitMatch.group(1)
+    else:
+        return ""
+
+# Strips away 4119 broken entries
+def checkMissingData(card:dict) -> bool:
+    importantTraits = ["name"
+                      ,"releaseDate"
+                      ,"collectorID"
+                      ,"imageSmall"
+                      ,"imageMedium"
+                      ,"imageLarge"
+                      ,"setAcro"
+                      ,"combinedMana"
+                      ,"oracleText"
+                      ,"setName"
+                      ,"setType"
+                      ,"rarity"
+                      ,"layout"
+                      ,"artist"
+                      ]
+    for trait in importantTraits:
+        if card[trait] == "":
+            return True
+    return False
+
+def parseCards(file:str):
+    cards = []
+    with open("data/"+file, 'r', encoding='cp437') as file:
+        lines = file.read().split('\n')
+        for line in lines:
+            if line == '[' or line == ']': continue # Skip first and last line
+
+            # Misc Traits
+            collectorID_ = getTrait("collector_number",line) # We can't make these ints due to "★"
+            flavorText_  = getTrait("flavor_text"     ,line).replace("\\", "") # Cleaning up strings
+            oracleText_  = getTrait("oracle_text"     ,line)        
+            #exactMana_   = getTrait("mana_cost"       ,line)
+            priceEUR_    = getTrait("eur"             ,line)
+            imageSmall_  = getTrait("small"           ,line)
+            imageNormal_ = getTrait("normal"           ,line)
+            imageLarge_  = getTrait("large"           ,line)
+            setAcro_     = getTrait("set"             ,line)
+            setName_     = getTrait("set_name"        ,line)
+            setType_     = getTrait("set_type"        ,line)
+            rarity_      = getTrait("rarity"          ,line)
+            layout_      = getTrait("layout"          ,line)
+            artist_      = getTrait("artist"          ,line)
+            name_        = getTrait("name"            ,line)
+            
+            # Types
+            typeLine     = getTrait("type_line"       ,line).encode().decode("unicode-escape").replace(u"Î\x93Ã\x87Ã¶", "-").split(" - ")
+            subTypes_ = []
+            if len(typeLine) != 1:
+                subTypes_ = typeLine[1].split(" ")
+            leftTypes = typeLine[0].split(" ")
+            validTypes = {"Creature"
+                        ,"Sorcery"
+                        ,"Summon"
+                        ,"Interrupt"
+                        ,"Instant"
+                        ,"Artifact"
+                        ,"Battle"
+                        ,"Conspiracy"
+                        ,"Emblem"
+                        ,"Enchantment"
+                        ,"Hero"
+                        ,"Land"
+                        ,"Phenomenon"
+                        ,"Plane"
+                        ,"Planeswalker"
+                        ,"Scheme"
+                        ,"Tribal"
+                        ,"Vanguard"
+                        }
+            types_ = []
+            superTypes_ = []
+            for type in leftTypes:
+                if type in validTypes:
+                    types_.append(type)
+                else:
+                    superTypes_.append(type)
+
+            # Keywords
+            keywords_ = []
+            keywordsMatch = re.search(r'"keywords":(\[.*?\]),', line)
+            if keywordsMatch != None:
+                keywords_ = eval(keywordsMatch.group(1))
+
+
+            # Combined Mana
+            combinedMana_         = 0.0
+            combinedManaMatch     = re.search(r'"cmc":(\d+\.\d?),', line)
+            if combinedManaMatch != None:
+                combinedMana_     = float(combinedManaMatch.group(1))
+
+            # Release Date
+            releaseDate_           = dt.datetime.min # Default value ?
+            dateMatch              = re.search(r'"released_at":"(\d+)-(\d+)-(\d+)",', line)
+            if dateMatch != None:
+                (year, month, day) = (int(dateMatch.group(1)), int(dateMatch.group(2)), int(dateMatch.group(3)))
+                releaseDate_       = dt.datetime(year, month, day)
+            
+            cardAttr = dict(
+                name         = name_,
+                releaseDate  = releaseDate_,
+                superTypes   = superTypes_,
+                types        = types_,
+                subTypes     = subTypes_,
+                keywords    = keywords_,
+            #    exactMana    = exactMana_,
+                combinedMana = combinedMana_,
+                oracleText   = oracleText_,
+                collectorID  = collectorID_,
+                flavorText   = flavorText_,
+                priceEUR     = priceEUR_,
+                imageSmall   = imageSmall_,
+                imageNormal  = imageNormal_,
+                imageLarge   = imageLarge_,
+                setAcro      = setAcro_,
+                setName      = setName_,
+                setType      = setType_,
+                rarity       = rarity_,
+                layout       = layout_,
+                artist       = artist_,
+            )
+            isBadCard = checkMissingData(cardAttr)
+            if isBadCard: continue  # This could be optimized so we early exit as soon as we receive a bad trait
+
+            cards.append(cardAttr)
+
+    # Sorting by date and then name to prepare for duplicate removal
+    sortedByDate = sorted(cards, key=lambda x: x["releaseDate"])
+    sortedByNameDate = sorted(cards, key=lambda x: x["name"])
+
+    # Removing 61571 duplicates, keeping only the original prints
+    noDupesCards = []
+    for x in range(len(sortedByNameDate)):
+        currentName = sortedByNameDate[x]["name"]
+        if (x != len(sortedByNameDate)-1):
+            nextName = sortedByNameDate[x+1]["name"]
+            if currentName == nextName: continue
+            else: noDupesCards.append(sortedByNameDate[x])
+        else: noDupesCards.append(sortedByNameDate[x])
+    return noDupesCards
--- backend/init_db.py
+++ backend/init_db.py
@@ -1,5 +1,6 @@
 import psycopg2
 import db
+from backend.cardParser import parseCards
 
 conn = psycopg2.connect(
     host=db.DB_HOST,
@@ -8,16 +9,62 @@ conn = psycopg2.connect(
     password=db.DB_PASSWORD)
 
 cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS cards;')
+cur.execute('DROP TABLE IF EXISTS Cards;')
 cur.execute('CREATE TABLE cards (id serial PRIMARY KEY,'
             'name varchar (150) NOT NULL,'
+            'releaseDate varchar (32) NOT NULL,'
+            'cmc INT NOT NULL,'
+            'oracle varchar (250) NOT NULL,'
+            'collectorID varchar (8) NOT NULL,'
+            'flavorText varchar (250) NOT NULL,'
+            'priceEUR varchar (8) NOT NULL,'
+            'imageSmall varchar (200) NOT NULL,'
+            'imageNormal varchar (200) NOT NULL,'
+            'imageLarge varchar (200) NOT NULL,'
+            'setAcro varchar (8) NOT NULL,'
+            'rarity varchar (16) NOT NULL,'
             'artist varchar (150) NOT NULL);'
             )
 
-cur.execute('INSERT INTO cards (name, artist)'
-            'VALUES (%s, %s)',
-            ('Sample card 1', 'Sample artist 1')
-            )
+def insertCard(card):
+    cur.execute('INSERT INTO Cards (name,'
+                                   'releaseDate,'
+                                   'cmc,'
+                                   'oracle,'
+                                   'collectorID,'
+                                   'flavorText,'
+                                   'priceEUR,'
+                                   'imageSmall,'
+                                   'imageNormal,'
+                                   'imageLarge,'
+                                   'setAcro,'
+                                   'rarity,'
+                                   'artist)'
+                'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                (card["name"]
+                ,card["releaseDate"]
+                ,card["combinedMana"]
+                ,card["oracleText"]
+                ,card["collectorID"]
+                ,card["flavorText"]
+                ,card["priceEUR"]
+                ,card["imageSmall"]
+                ,card["imageNormal"]
+                ,card["imageLarge"]
+                ,card["setAcro"]
+                ,card["rarity"]
+                ,card["artist"]
+                )
+        )
+
+
+map(insertCard, parseCards("rawCards.json"))
+
+
+#cur.execute('INSERT INTO Cards (name, artist)'
+#            'VALUES (%s, %s)',
+#            ('Sample card 1', 'Sample artist 1')
+#            )
 
 conn.commit()
 
