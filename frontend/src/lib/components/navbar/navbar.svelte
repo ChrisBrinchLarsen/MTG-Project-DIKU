de95be2f6d6390b441089f10aada0a6e654e024a
Martin Polley <martin.polley@lenus.io>
2024-05-30 15:39:55 +0200

    Add custom font.

--- frontend/src/lib/components/navbar/navbar.svelte
+++ frontend/src/lib/components/navbar/navbar.svelte
@@ -1,3 +1,3 @@
 <header class="my-6 flex flex-col items-center pb-8 pt-4">
-  <h1 class="text-4xl font-extrabold text-gray-200">Magic the Gathering'dle</h1>
+  <h1 class="text-5xl font-extrabold text-white">Magic the Gathering'dle</h1>
 </header>
