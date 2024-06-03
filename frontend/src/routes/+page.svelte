568f3295d8b2c262822ad0d249c36d9659f9aecd
Martin Polley <martin.polley@lenus.io>
2024-06-03 12:35:41 +0200

    Use shadcn button on homepage.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { goto } from '$app/navigation'
+  import { Button } from '$lib/components/ui/button'
 </script>
 
 <div class="mx-auto flex h-[60vh] max-w-sm flex-col justify-center">
-  <button on:click={() => goto('/game')} class="rounded-lg bg-gray-300 px-6 py-2">Play</button>
+  <Button on:click={() => goto('/game')} size="lg" class="text-xl">Play regular mode</Button>
 </div>
